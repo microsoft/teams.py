f50ccadbfbaaf04209f5b7cdcf4902620fef3e35
Lily Du <lilyyduu@gmail.com>
2025-06-10 08:59:13 -0700

    [common]: add logging module (#23)
    * add logging module
    
    * fix int level conversion to str
    
    ---------
    
    Co-authored-by: lilyydu <lilydu+odspmdb@microsoft.com>

---
+++ packages/common/src/common/logging/filter.py
@@ -0,0 +1,25 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
+
+from __future__ import annotations
+
+import logging
+import re
+
+
+class ConsoleFilter(logging.Filter):
+    
+    def __init__(self, pattern: str = '*'):
+        super().__init__()
+        self.pattern = self._parse_magic_expr(pattern)
+
+    def filter(self, record: logging.LogRecord) -> bool:
+        return bool(self.pattern.match(record.name))
+
+    @staticmethod
+    def _parse_magic_expr(pattern: str) -> re.Pattern:
+        pattern = pattern.replace('*', '.*')
+        return re.compile(f'^{pattern}$')
