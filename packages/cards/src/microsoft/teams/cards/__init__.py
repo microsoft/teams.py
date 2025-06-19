e329ebb85a51282def0b76ada96961afea82a3c3
lilydu <lilydu+odspmdb@microsoft.com>
2025-06-19 11:14:20 -0700

    remove contents in init

--- packages/cards/src/microsoft/teams/cards/__init__.py
+++ packages/cards/src/microsoft/teams/cards/__init__.py
@@ -2,8 +2,3 @@
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
-
-from .adaptive_card import AdaptiveCard
-
-# Combine all exports from submodules
-__all__ = ["AdaptiveCard"]
