195eb89626e83caec1b10beab5e642c26b5557a0
lilydu <lilydu+odspmdb@microsoft.com>
2025-06-20 15:55:24 -0700

    build fail for mod

--- packages/cards/src/microsoft/teams/cards/__init__.py
+++ packages/cards/src/microsoft/teams/cards/__init__.py
@@ -2,3 +2,8 @@
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
+
+from .adaptive_card import AdaptiveCard
+
+# Combine all exports from submodules
+__all__ = ["AdaptiveCard"]
