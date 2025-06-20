0c53f7c61a723a8b8707e8e928782c33d3290a78
lilydu <lilydu+odspmdb@microsoft.com>
2025-06-20 14:51:23 -0700

    add back init file

---
+++ packages/cards/src/microsoft/teams/cards/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
+from .core import AdaptiveCard
+
+# Combine all exports from submodules
+__all__ = [
+    "AdaptiveCard",
+]
