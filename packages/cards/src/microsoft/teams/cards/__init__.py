1516687159208699cc946512ca6e6d28cc35e6c3
lilydu <lilydu+odspmdb@microsoft.com>
2025-06-20 14:12:49 -0700

    move adaptive card

--- packages/cards/src/microsoft/teams/cards/__init__.py
+++ packages/cards/src/microsoft/teams/cards/__init__.py
@@ -2,7 +2,3 @@
 Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
-
-from .adaptive_card import AdaptiveCard
-
-__all__ = ["AdaptiveCard"]
--- packages/cards/src/microsoft/teams/cards/core.py
+++ packages/cards/src/microsoft/teams/cards/core.py
@@ -3,7 +3,6 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-from .adaptive_card import AdaptiveCard
 
-# Combine all exports from submodules
-__all__ = ["AdaptiveCard"]
+class AdaptiveCard:
+    pass
