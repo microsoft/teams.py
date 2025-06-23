840f2098ef34f529ee82b735fb28908972d69753
lilydu <lilydu+odspmdb@microsoft.com>
2025-06-23 09:12:47 -0700

    fix failing test

--- packages/cards/src/microsoft/teams/cards/adaptive_card.py
+++ packages/cards/src/microsoft/teams/cards/adaptive_card.py
@@ -3,6 +3,8 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
+from microsoft.teams.api.models.custom_base_model import CustomBaseModel
 
-class AdaptiveCard:
+
+class AdaptiveCard(CustomBaseModel):
     pass
