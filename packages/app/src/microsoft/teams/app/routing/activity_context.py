7e3353d886228212652f578017ada259feece5b7
heyitsaamir <ajawaid191@gmail.com>
2025-07-23 23:36:44 -0700

    fix tests

--- packages/app/src/microsoft/teams/app/routing/activity_context.py
+++ packages/app/src/microsoft/teams/app/routing/activity_context.py
@@ -1,3 +1,8 @@
+"""
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.
+"""
+
 from typing import Awaitable, Callable, Generic, Optional, TypeVar
 
 from microsoft.teams.api import ActivityBase
