d548c76f585e783ccb92567b15b0bfbd89da564c
heyitsaamir <ajawaid191@gmail.com>
2025-07-27 21:47:07 -0700

    fisx

--- packages/app/src/microsoft/teams/app/auth/__init__.py
+++ packages/app/src/microsoft/teams/app/auth/__init__.py
@@ -3,13 +3,10 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-from .jwt_middleware import JwtValidationMiddleware, create_jwt_validation_middleware
-from .service_token_validator import (
-    ServiceTokenValidator,
-)
+from .jwt_middleware import create_jwt_validation_middleware
+from .service_token_validator import ServiceTokenValidator
 
 __all__ = [
     "ServiceTokenValidator",
-    "JwtValidationMiddleware",
     "create_jwt_validation_middleware",
 ]
