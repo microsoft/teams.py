72c1210f60851d671f9e6fba70b6f1d938c73875
heyitsaamir <ajawaid191@gmail.com>
2025-08-20 16:18:18 -0700

    Fix

--- packages/app/src/microsoft/teams/app/app.py
+++ packages/app/src/microsoft/teams/app/app.py
@@ -12,15 +12,16 @@ from typing import Any, Callable, List, Optional, TypeVar, Union, cast, overload
 from dependency_injector import providers
 from dotenv import find_dotenv, load_dotenv
 from microsoft.teams.api import (
+    Account,
+    ActivityParams,
     ApiClient,
     ClientCredentials,
+    ConversationAccount,
     ConversationReference,
     Credentials,
     JsonWebToken,
+    MessageActivityInput,
 )
-from microsoft.teams.api.activities.message.message import MessageActivityInput
-from microsoft.teams.api.clients.conversation.activity import ActivityParams
-from microsoft.teams.api.models.account import Account, ConversationAccount
 from microsoft.teams.cards import AdaptiveCard
 from microsoft.teams.common import Client, ClientOptions, ConsoleLogger, EventEmitter, LocalStorage
 
