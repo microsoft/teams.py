55a9149c55a52fafb54a3ecfc0dbee2a8574cae1
lilydu <lilydu+odspmdb@microsoft.com>
2025-08-21 14:55:47 -0700

    add additional card actions

--- packages/cards/src/microsoft/teams/cards/__init__.py
+++ packages/cards/src/microsoft/teams/cards/__init__.py
@@ -3,11 +3,11 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-from . import common
-from .common import *  # noqa: F403
+from . import actions
+from .actions import *  # noqa: F403
 from .core import *
+from .serializable_object import SerializableObject
 
 # Combine all exports from submodules
-__all__: list[str] = []
-__all__.extend(common.__all__)
-# __all__.extend(actions.__all__)
+__all__: list[str] = ["SerializableObject"]
+__all__.extend(actions.__all__)
