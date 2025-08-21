f65d63ce7ce4a00044c988fde0bf4f798103420d
lilydu <lilydu+odspmdb@microsoft.com>
2025-08-21 15:00:46 -0700

    undo move of SerializableObject from core.py

--- packages/cards/src/microsoft/teams/cards/__init__.py
+++ packages/cards/src/microsoft/teams/cards/__init__.py
@@ -6,8 +6,7 @@ Licensed under the MIT License.
 from . import actions
 from .actions import *  # noqa: F403
 from .core import *
-from .serializable_object import SerializableObject
 
 # Combine all exports from submodules
-__all__: list[str] = ["SerializableObject"]
+__all__: list[str] = []
 __all__.extend(actions.__all__)
--- packages/cards/src/microsoft/teams/cards/core.py
+++ packages/cards/src/microsoft/teams/cards/core.py
@@ -8,9 +8,53 @@ Licensed under the MIT License.
 
 from typing import Any, Dict, List, Literal, Optional, Self, Union
 
-from pydantic import Field
+from pydantic import AliasGenerator, BaseModel, ConfigDict, Field
+from pydantic.alias_generators import to_camel
 
-from .serializable_object import SerializableObject
+
+class SerializableObject(BaseModel):
+    """Base class for all serializable objects."""
+
+    @staticmethod
+    def validation_alias_generator(field: str) -> str:
+        "Handles deserialization aliasing"
+
+        # Handle parameters that start with "@"
+        if field.startswith("at_"):
+            return f"@{field[3:]}"
+
+        # Handles from field which is a duplicate reserved internal name
+        if field == "from_":
+            return "from"
+
+        # All other fields are converted to camelCase
+        return to_camel(field)
+
+    @staticmethod
+    def serialization_alias_generator(field: str) -> str:
+        "Handles serialization aliasing and casing"
+
+        # Handle parameters that start with "@"
+        if field.startswith("at_"):
+            return f"@{field[3:]}"
+
+        # Handles from field which is a duplicate reserved internal name
+        if field == "from_":
+            return "from"
+
+        # All other fields are converted to camelCase
+        return to_camel(field)
+
+    model_config = ConfigDict(
+        populate_by_name=True,
+        serialize_by_alias=True,
+        validate_by_alias=True,
+        arbitrary_types_allowed=True,
+        extra="allow",
+        alias_generator=AliasGenerator(
+            validation_alias=validation_alias_generator, serialization_alias=serialization_alias_generator
+        ),
+    )
 
 
 class CardElement(SerializableObject):
