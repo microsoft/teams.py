94585198439e0112095fbf8e2a20ca8a8ca320b1
Lily Du <lilyyduu@gmail.com>
2025-09-10 15:33:31 -0700

    [common]: expose ConsoleLoggerOptions in init  (#144)
    export ConsoleLoggerOptions so devs can configure it
    
    Co-authored-by: lilydu <lilydu+odspmdb@microsoft.com>

--- packages/common/src/microsoft/teams/common/logging/__init__.py
+++ packages/common/src/microsoft/teams/common/logging/__init__.py
@@ -4,8 +4,8 @@ Licensed under the MIT License.
 """
 
 from .ansi import ANSI
-from .console import ConsoleLogger
+from .console import ConsoleLogger, ConsoleLoggerOptions
 from .filter import ConsoleFilter
 from .formatter import ConsoleFormatter
 
-__all__ = ["ANSI", "ConsoleLogger", "ConsoleFormatter", "ConsoleFilter"]
+__all__ = ["ANSI", "ConsoleLogger", "ConsoleFormatter", "ConsoleFilter", "ConsoleLoggerOptions"]
