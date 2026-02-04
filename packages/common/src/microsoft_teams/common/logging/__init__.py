7ac2243b1f0f9c375f4db7da145fa96b3dfeb083
lilydu <lilydu+odspmdb@microsoft.com>
2026-02-04 14:48:14 -0800

    remove general warning on init file

--- packages/common/src/microsoft_teams/common/logging/__init__.py
+++ packages/common/src/microsoft_teams/common/logging/__init__.py
@@ -3,19 +3,9 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-import warnings
-
 from .ansi import ANSI
 from .console import ConsoleLogger, ConsoleLoggerOptions
 from .filter import ConsoleFilter
 from .formatter import ConsoleFormatter
-
-# Issue deprecation warning
-warnings.warn(
-    "The 'ConsoleLogger' class is deprecated and will be removed in version 2.0.0 GA."
-    + " Please update your imports to use the standard Python logging library instead.",
-    DeprecationWarning,
-    stacklevel=2,
-)
 
 __all__ = ["ANSI", "ConsoleFormatter", "ConsoleFilter", "ConsoleLogger", "ConsoleLoggerOptions"]
