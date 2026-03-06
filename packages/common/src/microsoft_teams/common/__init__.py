5ec67c1564fb72423f8f96e68c4dca9552b96841
Corina Gum <>
2026-03-06 14:25:42 -0800

    Mark reactions and targeted messages as preview

--- packages/common/src/microsoft_teams/common/__init__.py
+++ packages/common/src/microsoft_teams/common/__init__.py
@@ -5,12 +5,13 @@ Licensed under the MIT License.
 
 from . import events, http, logging, storage  # noqa: E402
 from .events import *  # noqa: F401, F402, F403
+from .experimental import ExperimentalWarning, experimental
 from .http import *  # noqa: F401, F402, F403
 from .logging import *  # noqa: F401, F402, F403
 from .storage import *  # noqa: F401, F402, F403
 
 # Combine all exports from submodules
-__all__: list[str] = []
+__all__: list[str] = ["ExperimentalWarning", "experimental"]
 __all__.extend(events.__all__)
 __all__.extend(http.__all__)
 __all__.extend(logging.__all__)
