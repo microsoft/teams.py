951b3bb53edf83c2115295c7f205290bc3951878
lilydu <lilydu+odspmdb@microsoft.com>
2026-04-01 13:27:36 -0700

    rename to .open_dialog_data

--- packages/cards/src/microsoft_teams/cards/utilities/__init__.py
+++ packages/cards/src/microsoft_teams/cards/utilities/__init__.py
@@ -3,7 +3,7 @@ Copyright (c) Microsoft Corporation. All rights reserved.
 Licensed under the MIT License.
 """
 
-from .open_dialog import OpenDialogData
+from .open_dialog_data import OpenDialogData
 from .submit_data import SubmitData
 
 __all__ = ["OpenDialogData", "SubmitData"]
